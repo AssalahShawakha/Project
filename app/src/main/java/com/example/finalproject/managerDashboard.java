5c92816a630980100e53267230da816f5c8493a6
Assalah <assalahshawakha@gmail.com>
2025-01-09 12:15:16 +0200

    new

--- app/src/main/java/com/example/finalproject/managerDashboard.java
+++ app/src/main/java/com/example/finalproject/managerDashboard.java
@@ -1,35 +1,78 @@
 package com.example.finalproject;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 public class managerDashboard extends AppCompatActivity {
 
+    private TaskAdapter adapter;
+    private List<Task> taskList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager_dashboard);
 
-        RecyclerView RecycleView = findViewById(R.id.RecycleView);
-        RecycleView.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerView recyclerView = findViewById(R.id.RecycleView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        // Sample Data
-        List<Task> taskList = new ArrayList<>();
-        taskList.add(new Task("Team Meeting", "Group discussion for the new product", "10:00 AM", 48));
-        taskList.add(new Task("UI Design", "Make a homepage for the app", "11:00 AM", 30));
+        adapter = new TaskAdapter(taskList);
+        recyclerView.setAdapter(adapter);
 
-        TaskAdapter adapter = new TaskAdapter(taskList);
-        RecycleView.setAdapter(adapter);
+        new FetchTasks().execute();
+    }
+
+
+    private class FetchTasks extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                URL url = new URL("http://192.168.1.186/getTasks.php");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+                return response.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            try {
+                JSONArray jsonArray = new JSONArray(response);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject taskObject = jsonArray.getJSONObject(i);
+                    String title = taskObject.getString("title");
+                    String description = taskObject.getString("description");
+                    String time = taskObject.getString("time");
+                    int progress = taskObject.getInt("progress");
+
+                    taskList.add(new Task(title, description, time, progress));
+                }
+                adapter.notifyDataSetChanged();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
